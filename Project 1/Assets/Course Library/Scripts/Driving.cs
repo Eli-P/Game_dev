5b90656d1579d244c8d2e5675432c0cf9c1904c6
Eli <eli.p561@stu.nebo.edu>
2020-09-16 08:40:42 -0600

    First Commit

---
+++ Project 1/Assets/Course Library/Scripts/Driving.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driving: MonoBehaviour
+{
+    public float steering;
+    public int speed;
+    public float acceleration;
+    public float fb;
+    public float rl;
+    public float deacceleration;
+    public float flip;
+    public string Horizontal;
+    public string Vertical;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fb = Input.GetAxis(Vertical);
+        rl = Input.GetAxis(Horizontal);
+        flip = Input.GetAxis("Jump");
+        if (flip == 1)
+        {
+            gameObject.transform.eulerAngles.Set(0, 0, 0);
+        }
+        if (fb > 0 && acceleration <= 5)
+        {
+            acceleration += (fb / 12);
+        }else if (fb < 0 && acceleration >= -.5)
+        {
+            acceleration += (fb / 2);
+        }else
+        {
+            acceleration /= deacceleration;
+        }
+        if (steering > 20)
+        {
+            steering = 20;
+        }else if (steering < -20)
+        {
+            steering = -20;
+        }
+        else if (steering <= 20)
+        {
+            steering += rl / 5;
+        }
+        else if (steering >= -20)
+        {
+            steering += rl / 5;
+        }
+        if (acceleration > 0 && acceleration < .0001)
+        {
+            acceleration = 0;
+        }else if (acceleration < 0 && acceleration > -.0001)
+        {
+            acceleration = 0;
+        }
+        else if (acceleration > 0)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * acceleration * speed);
+            if (acceleration > .1)
+            {
+                transform.Rotate(Vector3.up * Time.deltaTime * steering);
+            }
+        }
+        else if(acceleration < 0)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * acceleration * speed);
+            if (acceleration < .1)
+            {
+                transform.Rotate(Vector3.up * Time.deltaTime * steering * -1);
+            }
+        }
+    }
+}
---
+++ Project 1/Assets/Course Library/Scripts/MAV.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MAV : MonoBehaviour
+{
+    public float speed;
+    public float turnSpeed;
+    public float range;
+    public GameObject player;
+    public float playerDist;
+    public float x;
+    public float y;
+    public float pX;
+    public float pY;
+    public float mX;
+    public float mY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mX = transform.position.x;
+        mY = transform.position.y;
+        pX = player.transform.position.x;
+        pY = player.transform.position.y;
+        if (mX - pX != 0)
+        {
+            if (mX - pX < 0)
+            {
+                x = pX - mX;
+            }
+            else
+            {
+                x = mX - pX;
+            }
+        }
+        else
+        {
+            x = 0;
+        }
+        if (mY - pY != 0)
+        {
+            if (mY - pY < 0)
+            {
+                y = pY - mY;
+            }
+            else
+            {
+                y = mY - pY;
+            }
+        }
+        else
+        {
+            x = 0;
+        }
+        x *= x;
+        y *= y;
+        
+    }
+}
